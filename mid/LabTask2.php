99ba2ca09821224d68b19c9fc3b7b6937490c158
Ansar31337 <ansar31337@gmail.com>
2025-07-23 12:52:21 +0600

    Lab Task 2

---
+++ mid/LabTask2.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html>
+
+<head>
+    <title>Bank Management System</title>
+    <style> 
+           body {
+               background-color: #f5f5f5;
+               font-family: Arial, sans-serif;
+               margin: 0;
+               padding: 0;
+               height: 100vh;
+           }
+           h1, h2{
+            text-align: center;
+            color: darkgreen;
+            margin: 10px 0;
+           }
+           .form-box {
+               width: 480px;
+               background-color: white;
+               margin: 10px auto;
+               padding: 15px;
+               border: 1px solid #ccc;
+               border-radius: 6px;
+           }
+           table {
+              width: 100%;
+           }
+           td {
+               padding: 6px
+           }
+           td { 
+                padding: 6px;
+                font-size: 14px;
+           }
